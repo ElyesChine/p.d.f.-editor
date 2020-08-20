6ecb5b23aa46fac460f57ae84eb29ff8aa565df1
elyes monster <chine.elyes@gmail.com>
2020-08-20 04:45:03 +0200

    force refresh page after first call to the page

--- js.js
+++ js.js
@@ -1,12 +1,11 @@
 console.log('start js.js');
 const { PDFDocument } = PDFLib
-var imageContent;
+var imageContent,pagesss;
 var countImages = -1;
 
 //télécharger le nouveau fichier en itérant sur l'ensmeble des images tout en regardant les changements de leurs posisitions pour savoir s'il faut les ajouter au fichier final
 async function save() {
-    // const jpgUrl;
-    //
+
     const pdfDoc = await PDFDocument.load(base64);
     console.log(pdfDoc.getPages());
     let w;
@@ -19,6 +18,7 @@ async function save() {
             console.log(e);
             let nombrePage = Math.floor(e.yPos / longueurPage);
             console.log(e.yPos);
+            pagesss = pagesss+ "" + e.reference + "->" + nombrePage;
             var page = pdfDoc.getPages()[nombrePage];
             h = page.getHeight();
             imageDims = {height: 0, width: 0};
@@ -28,8 +28,8 @@ async function save() {
 
                 console.log(imageDims.height);
                 console.log(imageDims.width);
-                imageDims.height = parseInt(document.getElementById('signature' + e.reference).style.height.substring(0,3)) * (1 / 2)
-                imageDims.width = parseInt(document.getElementById('signature' + e.reference).style.width.substring(0,3)) * (1 / 2)
+                imageDims.height = parseInt(document.getElementById('signature' + e.reference).style.height.substring(0, 3)) * (1 / 2)
+                imageDims.width = parseInt(document.getElementById('signature' + e.reference).style.width.substring(0, 3)) * (1 / 2)
 
                 const longueurPage = $('#canvas1').height();
 
@@ -41,8 +41,8 @@ async function save() {
                 imageBytes = _base64ToArrayBuffer(e.imageContent.substring(e.imageContent.search("base64,") + 7));
                 image = await pdfDoc.embedPng(imageBytes);
                 /*imageDims = image.scale(0.5);*/
-                imageDims.height = parseInt(document.getElementById('signature' + e.reference).style.height.substring(0,3)) * (1 / 2)
-                imageDims.width = parseInt(document.getElementById('signature' + e.reference).style.width.substring(0,3)) * (1 / 2)
+                imageDims.height = parseInt(document.getElementById('signature' + e.reference).style.height.substring(0, 3)) * (1 / 2)
+                imageDims.width = parseInt(document.getElementById('signature' + e.reference).style.width.substring(0, 3)) * (1 / 2)
 
                 const longueurPage = $('#canvas1').height();
 
@@ -50,9 +50,9 @@ async function save() {
                 xValue = e.xPos * (395.5 / 595);
                 yValue = h - imageDims.height * 462.5 / 350 - e.yPos * (397.5 / 595);
             }
-            console.log(parseInt(document.getElementById('signature' + e.reference).style.height.substring(0,3)) * (1 / 2));
-            imageDims.height = parseInt(document.getElementById('signature' + e.reference).style.height.substring(0,3)) * (1 / 2)
-            imageDims.width = parseInt(document.getElementById('signature' + e.reference).style.width.substring(0,3)) * (1 / 2)
+            console.log(parseInt(document.getElementById('signature' + e.reference).style.height.substring(0, 3)) * (1 / 2));
+            imageDims.height = parseInt(document.getElementById('signature' + e.reference).style.height.substring(0, 3)) * (1 / 2)
+            imageDims.width = parseInt(document.getElementById('signature' + e.reference).style.width.substring(0, 3)) * (1 / 2)
             console.log(xValue);
 
             page.drawImage(image, {
@@ -68,16 +68,20 @@ async function save() {
     download(pdfBytes, "pdf-lib_image_embedding.pdf", "application/pdf");
 }
 //récupérer les images persistées au stockage local du navigateur
+
 var keys = Object.keys(localStorage), i = keys.length;
 var images = [];
-
-while ( i-- ) {
+var s=0;
+var widths
+let w;
+while (i--) {
     let image = JSON.parse(localStorage.getItem(keys[i]));
     image.id = keys[i];
-    images.push( JSON.parse(localStorage.getItem(keys[i]))) ;
+    /*images.push( JSON.parse(localStorage.getItem(keys[i]))) ;
     console.log(countImages++);
     img = document.createElement("img");
     let e = document.createElement("span");
+    e.id="span"+countImages;
     img.id = 'signature'+countImages;
     console.log("**");
     const reference = countImages;
@@ -85,26 +89,32 @@ while ( i-- ) {
     img.src = image.imageContent;
     $(function() {
         $(document.getElementById('signature'+reference)).resizable();
-    });
+    });*/
     input = document.createElement("input");
     input.type = "submit";
-    input.value ="delete";
+    input.value = "delete";
     input.id = keys[i];
-    const id=keys[i];
-    input.onclick =() =>{
-        let element = document.getElementById('signature'+reference);
-        element.parentNode.removeChild(element);
-        element = document.getElementById(id);
-        element.parentNode.removeChild(element);
+    const id = keys[i];
+    const key = new String(+ new Date())
+    input.onclick = () => {
+        // // /*let element = document.getElementById('signature' + reference);*/
+        let elements = document.getElementsByClassName(""+key);
         localStorage.removeItem(id);
+        for (var i = 0; i < elements.length; i++) {
+            console.log(elements[i].id);
+            element = document.getElementById(elements[i].id);
+            element.textContent = '';
+            //elements[i].parentNode.removeChild(elements[i]);
+        }element = document.getElementById(id);
+        element.parentNode.removeChild(element);element.textContent="";
     };
-    console.log(img.naturalHeight);
-    input.style.marginTop=new String(img.naturalHeight/2);
-    input.style.marginLeft="15px";
-    input.style.marginRight="100px";
+    /*console.log(img.naturalHeight);
+    *//*input.style.marginTop=new String(img.naturalHeight/2);*//**/
+    input.style.marginLeft="0px";
+    /*input.style.marginRight="100px";*/
     input.className="btn btn-danger";
-    input.style.position="absolute";
-    images[reference].reference=reference;
+    /*input.style.position="absolute";
+    /*images[reference].reference=reference;
     $(e).draggable({
         start: function() {
 
@@ -115,7 +125,7 @@ while ( i-- ) {
             offset = $(this).offset();
             xPos = offset.left - leftSpace.left;
             yPos = offset.top - getPosition(document.getElementById("canvas1")).y
-            /* leftSpace.top - 350*/   ;
+            /* leftSpace.top - 350*/   /*;
             images[reference].xPos = xPos;
             console.log(xPos);
             images[reference].yPos = yPos;
@@ -128,9 +138,70 @@ while ( i-- ) {
         }
     });
 
-    e.appendChild(img);
-    document.getElementById("bibliotheque").appendChild(e);
-    document.getElementById("bibliotheque").appendChild(input);
+    //e.appendChild(img);
+    //document.getElementById("bibliotheque").appendChild(e);*/
+    //document.getElementById("bibliotheque").appendChild(input);
+    for (let j = 0; j < 9; j++) {
+        images.push(JSON.parse(localStorage.getItem(keys[i])));
+        let img2 = document.createElement("img");
+        img2.src = image.imageContent;
+        let idOfSpan  = new String(+ new Date());
+        let e2 = document.createElement("span");e2.className=key;e2.id=new String(+ new Date());
+        countImages = countImages + 1;
+        img2.id = 'signature' + countImages;
+        const reference2 = countImages;
+        console.log(image.imageContent);
+        img2.id = "signature" + countImages;
+        console.log(w=img2.naturalWidth);//console.log(reference)
+        // img.src = image.imageContent;
+        $(e2).css({
+            "position": "absolute",
+            "left": s
+        }).appendTo(document.getElementById("bibliotheque"));
+        $(img2).css({
+            /*"position" : "absolute",*/
+            /*"left"     : $("#"+"signature"+reference).position().left,
+            "top"      : $("#"+"signature"+reference).position().top*/
+        }).appendTo(e2);
+        $(function () {
+            $(img2).resizable();
+        });
+        images[reference2].reference = reference2;
+        $(e2).draggable({
+            start: function () {
+
+            },
+            drag: function (e) {
+                console.log('start drag');
+                console.log(reference2);
+                offset = $(this).offset();
+                xPos = offset.left - leftSpace.left;
+                yPos = offset.top - getPosition(document.getElementById("canvas1")).y
+                /* leftSpace.top - 350*/;
+                images[reference2].xPos = xPos;
+                console.log(xPos);
+                images[reference2].yPos = yPos;
+                console.log(yPos);
+                images[reference2].height = document.getElementById("signature" + reference2).style.height;
+                images[reference2].width = document.getElementById("signature" + reference2).style.width;
+                console.log('end drag')
+            },
+            stop: function () {
+            }
+        });
+        input.style.marginTop=new String(img2.naturalHeight/2);
+        w=document.getElementById("signature" + reference2);
+        console.log(w);
+    }
+    w=w.width;
+    console.log(w);
+    s = s + w + 25;
+    $(input).css({
+        "position": "absolute",
+        "left": s
+    }).appendTo(document.getElementById("bibliotheque"));
+    s = s + 95;
+
 }
 
 var h ;
@@ -201,41 +272,55 @@ var type , imageBase64;
 function readURL(input) {
     if (input.files && input.files[0]) {
         var reader = new FileReader();
-        img = document.createElement("img");
         reader.onload = function (e) {
-            imageContent = e.target.result;
-            console.log(imageContent);
-            countImages++;
-            const reference = countImages;
+            imageContent = e.target.result;localStorage.setItem(+ new Date(),JSON.stringify({type: (type = document.getElementById('file').files[0].type), imageContent: imageContent}));
+
+            let image = {type: (type = document.getElementById('file').files[0].type), imageContent: imageContent};
+            p=+ new Date();
+            image.id = p
+            ;
+            /*images.push( JSON.parse(localStorage.getItem(keys[i]))) ;
+            console.log(countImages++);
+            img = document.createElement("img");
+            let e = document.createElement("span");
+            e.id="span"+countImages;
             img.id = 'signature'+countImages;
-            img.src = imageContent;
-            document.getElementById("bibliotheque").appendChild(img);
-            type = document.getElementById('file').files[0].type;
-            imageBase64 = this.result;
-            base64String = this.result.substring(this.result.search("base64,")+7);
-            arrayBuffer = _base64ToArrayBuffer(this.result.substring(this.result.search("base64,")+7));
-            //imageContent = data:(type);base64,(encoded image)
-            let image = {type: type, imageContent: imageContent };
-            images.push(image);
-            key = new String(+ new Date())
-            localStorage.setItem(key,JSON.stringify(image));
-            console.log("hhh");
+            console.log("**");
+            const reference = countImages;
+            console.log(image.imageContent);
+            img.src = image.imageContent;
+            $(function() {
+                $(document.getElementById('signature'+reference)).resizable();
+            });*/
             input = document.createElement("input");
             input.type = "submit";
-            input.value ="delete";
-            input.id = key;
-            const id=key;
-            input.onclick =() =>{
-                let element = document.getElementById('signature'+reference);
-                element.parentNode.removeChild(element);
+            input.value = "delete";
+            input.id = p;
+            const id = p;
+            const key = new String(+new Date())
+            input.onclick = () => {
+                // // /*let element = document.getElementById('signature' + reference);*/
+                let elements = document.getElementsByClassName("" + key);
+                localStorage.removeItem(id);
+                for (var i = 0; i < elements.length; i++) {
+                    console.log(elements[i].id);
+                    element = document.getElementById(elements[i].id);
+                    element.textContent = '';
+                    //elements[i].parentNode.removeChild(elements[i]);
+                }
                 element = document.getElementById(id);
                 element.parentNode.removeChild(element);
-                localStorage.removeItem(id);
+                element.textContent = "";
+                location.reload();
             };
-            input.style.marginRight="15px";
-            input.className="btn btn-danger";
-            document.getElementById("bibliotheque").appendChild(input);
-            $(img).draggable({
+            /*console.log(img.naturalHeight);
+            *//*input.style.marginTop=new String(img.naturalHeight/2);*//**/
+            input.style.marginLeft = "0px";
+            /*input.style.marginRight="100px";*/
+            input.className = "btn btn-danger";
+            /*input.style.position="absolute";
+            /*images[reference].reference=reference;
+            $(e).draggable({
                 start: function() {
 
                 },
@@ -245,16 +330,87 @@ function readURL(input) {
                     offset = $(this).offset();
                     xPos = offset.left - leftSpace.left;
                     yPos = offset.top - getPosition(document.getElementById("canvas1")).y
-                    /* leftSpace.top - 350*/   ;
-                    images[reference].xPos = xPos;
-                    console.log(xPos);
-                    images[reference].yPos = yPos;
-                    console.log(yPos);
-                    console.log('end drag')
-                },
-                stop: function() {
-                }
-            });
+                    /* leftSpace.top - 350*/   /*;
+            images[reference].xPos = xPos;
+            console.log(xPos);
+            images[reference].yPos = yPos;
+            console.log(yPos);
+            images[reference].height=document.getElementById("signature"+reference).style.height;
+            images[reference].width=document.getElementById("signature"+reference).style.width;
+            console.log('end drag')
+        },
+        stop: function() {
+        }
+    });
+
+    //e.appendChild(img);
+    //document.getElementById("bibliotheque").appendChild(e);*/
+            //document.getElementById("bibliotheque").appendChild(input);
+            for (let j = 0; j < 9; j++) {
+                images.push(image);
+                let img2 = document.createElement("img");
+                img2.src = image.imageContent;
+                let idOfSpan = new String(+new Date());
+                let e2 = document.createElement("span");
+                e2.className = key;
+                e2.id = new String(+new Date());
+                countImages = countImages + 1;
+                img2.id = 'signature' + countImages;
+                const reference2 = countImages;
+                console.log(image.imageContent);
+                img2.id = "signature" + countImages;
+                console.log(w = img2.naturalWidth);//console.log(reference)
+                // img.src = image.imageContent;
+                $(e2).css({
+                    "position": "absolute",
+                    "left": s
+                }).appendTo(document.getElementById("bibliotheque"));
+                $(img2).css({
+                    /*"position" : "absolute",*/
+                    /*"left"     : $("#"+"signature"+reference).position().left,
+                    "top"      : $("#"+"signature"+reference).position().top*/
+                }).appendTo(e2);
+                $(function () {
+                    $(img2).resizable();
+                });
+                images[reference2].reference = reference2;
+                $(e2).draggable({
+                    start: function () {
+
+                    },
+                    drag: function (e) {
+                        console.log('start drag');
+                        console.log(reference2);
+                        offset = $(this).offset();
+                        xPos = offset.left - leftSpace.left;
+                        yPos = offset.top - getPosition(document.getElementById("canvas1")).y
+                        /* leftSpace.top - 350*/;
+                        images[reference2].xPos = xPos;
+                        console.log(xPos);
+                        images[reference2].yPos = yPos;
+                        console.log(yPos);
+                        images[reference2].height = document.getElementById("signature" + reference2).style.height;
+                        images[reference2].width = document.getElementById("signature" + reference2).style.width;
+                        console.log('end drag')
+                    },
+                    stop: function () {
+                    }
+                });
+                input.style.marginTop = new String(img2.naturalHeight / 2);
+                w = document.getElementById("signature" + reference2);
+            }
+            console.log(w);
+            w = w.width;
+            console.log(w);
+            s = s + w + 25;
+            $(input).css({
+                "position": "absolute",
+                "left": s
+            }).appendTo(document.getElementById("bibliotheque"));
+            s = s + 95;
+
+
+
         };
         reader.readAsDataURL(input.files[0]);
 
@@ -279,7 +435,7 @@ var pdf ;
 var base64;
 document.getElementById('fileInput').onchange = function () {
     let selectedFile = document.getElementById("fileInput").files;
-    //Check File is not Empty
+    //Check File is n²ot Empty
     if (selectedFile.length > 0) {
         // Select the very first file from list
         var fileToLoad = selectedFile[0];
